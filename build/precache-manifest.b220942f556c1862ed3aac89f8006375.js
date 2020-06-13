468d5fa917bbb9c6bb9344955e265446bfced615
Shikhar Bhardwaj <shikharbhardwajbs@gmail.com>
2020-06-13 17:32:19 +0530

    Build directory of project

---
+++ build/precache-manifest.b220942f556c1862ed3aac89f8006375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b34cbdd9484eff00864e8ea23dd6af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199ff09c920b53026c69",
+    "url": "/static/css/main.2583b1b1.chunk.css"
+  },
+  {
+    "revision": "b5e955e524f02234d7d3",
+    "url": "/static/js/2.d7367ce8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7367ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199ff09c920b53026c69",
+    "url": "/static/js/main.517956bf.chunk.js"
+  },
+  {
+    "revision": "cb22fc6fb7326d6a9cf5",
+    "url": "/static/js/runtime-main.bb0492ed.js"
+  }
+]);
